2edc2e397086da559a9b9ebb9e2414de98945ba7
alejandro <alejandro.paredes@iesdoctorbalmis.com>
2024-02-21 12:34:56 +0100

    News-details en camino

--- VoluntApp_IONIC/src/app/app.routes.ts
+++ VoluntApp_IONIC/src/app/app.routes.ts
@@ -8,13 +8,6 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./components/menu/tabs-routing.module').then( m => m.TabsPageRoutingModule)
-  },  {
-    path: 'news-details',
-    loadComponent: () => import('./components/news-details/news-details.page').then( m => m.NewsDetailsPage)
-  },
-  {
-    path: 'news-card',
-    loadComponent: () => import('./components/news-card/news-card.page').then( m => m.NewsCardPage)
   },
 
   
