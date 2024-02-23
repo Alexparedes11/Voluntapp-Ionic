a736bc32118fb0f3bc570cfc904810ea3f450cf7
Saul Valcarcel Picapiedra <saul.valcarcel@iesdoctorbalmis.com>
2024-02-23 09:42:27 +0100

    Actualizado la dirección IP del servidor en environment.prod.ts a 'http://4.180.125.221'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     accessToken: 'pk.eyJ1Ijoibm9lbGJhbG1pcyIsImEiOiJjbHNhZTNqbmswMnVlMmpvMWxiYTIwN2xwIn0.S0Cho6IO0XV7wvhE7v68sQ'
 },
 server: {
-    ip: 'http://localhost',
+    ip: 'http://4.180.125.221',
     port: '9000'
 }
 };
